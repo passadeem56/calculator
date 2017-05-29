47a0add85c549065a9f9558af0208a88fbeb2a40
passadeem56 <passadeem56@email.nu.ac.th>
2017-05-29 15:42:54 +0700

    Initial commit

---
+++ src/calculator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace passadeem56\Calculator;
+
+class Calculator
+{
+    public function add($first_num, $second_num)
+    {
+        return $first_num + $second_num;
+    }
+    public function subtract($first_num, $second_num)
+    {
+        return $first_num - $second_num;
+    }
+    public function multiply($first_num, $second_num)
+    {
+        return $first_num * $second_num;
+    }
+    public function divide($first_num, $second_num)
+    {
+        return $first_num / $second_num;
+    }
+}
